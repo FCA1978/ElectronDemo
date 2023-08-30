338919bf7a4276f50f995f58cf9e0dc52f68bee4
FCA1978 <292409835@qq.com>
2023-08-30 17:32:52 +0800

    新增demo样例

---
+++ main.js
@@ -0,0 +1,29 @@
+// 使用CommonJS语法导入了两个Electron模块
+// app 应用程序的事件生命周期
+// BrowserWindow 负责创建和管理应用窗口
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
